9e74d17b7bd77bf1a3d4ce36a5a5b766919c8aba
Jamie <jamieyw.hu@gmail.com>
2025-06-11 21:51:04 -0700

    finish lab5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,13 +1,25 @@
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+import { Container } from "react-bootstrap";
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+
 export default function Lab5() {
   return (
-    <div id="wd-lab5">
-      <h2>Lab 5</h2>
-      <div className="list-group">
-        <a href="http://localhost:4000/lab5/welcome"
-            className="list-group-item">
-          Welcome
-        </a>
-      </div><hr />
-    </div>
+    <Container>
+      <div id="wd-lab5">
+        <h2>Lab 5</h2>
+        <div className="list-group">
+          <a href={`${REMOTE_SERVER}/lab5/welcome`}
+              className="list-group-item">
+            Welcome
+          </a>
+        </div><hr />
+
+        <EnvironmentVariables />
+        <PathParameters />
+        <QueryParameters />
+      </div>
+    </Container>
   );
 }
