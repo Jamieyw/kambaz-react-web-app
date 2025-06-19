f128292553691b29a72cc5cb4493b1ef9398d39a
Jamie <jamieyw.hu@gmail.com>
2025-06-18 22:13:19 -0700

    update kambaz

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -25,6 +25,7 @@ export default function Account() {
             <Route path="/Profile" element={<Profile />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Users" element={<Users />} />
+            <Route path="/Users/:uid" element={<Users />} />
           </Routes>
         </div>
       </div>
--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,9 +1,12 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
+import { Link } from "react-router";
 
 export default function PeopleTable({users = []}: {users?: any[]}) {
   return (
     <div id="wd-people-table">
+      <PeopleDetails />
       <Table striped>
         <thead>
           <tr>
@@ -15,15 +18,17 @@ export default function PeopleTable({users = []}: {users?: any[]}) {
           {users.map((user: any) => (
               <tr key={user._id}>
                 <td className="wd-full-name text-nowrap">
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName}</span>{" "}
-                <span className="wd-last-name">{user.lastName}</span>
-              </td>
-              <td className="wd-login-id">{user.loginId}</td>
-              <td className="wd-section">{user.section}</td>
-              <td className="wd-role">{user.role}</td>
-              <td className="wd-last-activity">{user.lastActivity}</td>
-              <td className="wd-total-activity">{user.totalActivity}</td>
+                  <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}</span>{" "}
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </Link>
+                </td>
+                <td className="wd-login-id">{user.loginId}</td>
+                <td className="wd-section">{user.section}</td>
+                <td className="wd-role">{user.role}</td>
+                <td className="wd-last-activity">{user.lastActivity}</td>
+                <td className="wd-total-activity">{user.totalActivity}</td>
               </tr>
             ))}
         </tbody>
